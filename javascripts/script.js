a116dd03bfef6c0f02ba78ce22b95cf3f487b880
aquarioos <aquarioos@yandex.ru>
2017-08-28 22:02:39 +0200

    minimalism!

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,62 +1,11 @@
-const DURATION = 1;
-const INTERVAL = 5;
-const DEFAULT_EASE = Power4.easeIn;
-const EXTRA_EASE = Bounce.easeOut;
+const TOGGLE_FONT = {
+    'font-black': 'font-white',
+    'font-white': 'font-black'
+};
 
-function permutations(array) {
-    var newArray = [];
-    var size = array.length;
-    while(size > 0) {
-        var i = Math.floor(Math.random() * size);
-        newArray.push(array[i]);
-        array[i] = array[size - 1];
-        --size;
-    }
-    return newArray;
-}
+function appear() {
+    var nameSurnameClassName = document.getElementById('name-surname').getAttribute('class');
+    document.getElementById('name-surname').setAttribute('class', TOGGLE_FONT[nameSurnameClassName]);
 
-function daniel() {
-    var body = jQuery("body");
-    var menu = jQuery(".menu_header, .menu_item");
-    TweenMax.staggerTo(menu, DURATION, {left: "-100%", ease: DEFAULT_EASE}, DURATION / 2);
-    setTimeout(function() {
-        menu.remove();
-    }, 1000 * 1.5 * DURATION);
-    setTimeout(function() {
-        var permPhotos = permutations(DANIEL_PHOTOS);
-        var photo = jQuery("<img/>", {class: "photo", src: permPhotos[0], style: "opacity: 0;"});
-        photo.appendTo(body);
-        TweenMax.to(photo, DURATION, {opacity: 1});
-        var i = 1;
-        setInterval(function() {
-            var oldPhoto = jQuery(".photo");
-            TweenMax.to(oldPhoto, DURATION, {left: "-100%"}, DURATION);
-            setTimeout(function() {
-                oldPhoto.remove();
-            }, 1000 * 2 * DURATION);
-            var photo = jQuery("<img/>", {class: "photo", src: permPhotos[i]});
-            photo.appendTo(body);
-            TweenMax.from(photo, DURATION, {left: "100%"}, DURATION);
-            ++i;
-            i %= permPhotos.length;
-        }, 1000 * INTERVAL);
-    }, 1000 * 1.5 * DURATION);
-}
-
-function init() {
-    var body = jQuery("body");
-    jQuery("<div/>", {
-        class: "menu_header",
-        text: "TARANTA..."
-    }).appendTo(body);
-    for(var i in ITEMS) {
-        var item = jQuery("<div/>", {
-            class: "menu_item",
-            style: "top: 100%",
-            html: ITEMS[i].html,
-            onclick: ITEMS[i].onclick
-        });
-        item.appendTo(body);
-        TweenMax.to(item, DURATION, {ease: EXTRA_EASE, top: ITEMS[i].top});
-    }
+    document.getElementById('bg-image').setAttribute('class', 'bg-image-blurred');
 }
