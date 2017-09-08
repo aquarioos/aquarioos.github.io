772899111066202b48ad00e65fd83c82b00addaa
Aquarioos <dtaranta@gmail.com>
2017-09-09 00:48:30 +0200

    animated letters

--- javascripts/script.js
+++ javascripts/script.js
@@ -3,9 +3,45 @@ const TOGGLE_FONT = {
     'font-white': 'font-black'
 };
 
-function appear() {
-    var nameSurnameClassName = document.getElementById('name-surname').getAttribute('class');
-    document.getElementById('name-surname').setAttribute('class', TOGGLE_FONT[nameSurnameClassName]);
+function initialAnimation() {
+  // appearElements(['console'], ['bg', 'title'], 1, 2);
+  // add shadow
+  var initialTimeline = new TimelineLite();
+  initialTimeline.add(
+    TweenLite.from('#bg-image img', 2,
+      {'min-height': '120vh', 'min-width': '120vw', ease: Bounce.easeOut})
+  );
+  initialTimeline.add(
+    TweenLite.to('#bg-image', 0.2, {delay: 0.5, opacity: 0.5, contrast: 0.75})
+  );
+  // initialTimeline.add(
+  //   TweenLite.to(
+  //     '#toggle-button', 1,
+  //     {opacity: 1}
+  //   )
+  // );
+  initialTimeline.add(
+    TweenLite.to('#name-surname', 1, {opacity: 1})
+  );
 
-    document.getElementById('bg-image').setAttribute('class', 'bg-image-blurred');
+  for (chr of 'Daniel TARANTA') {
+    initialTimeline.add(
+      TweenLite.delayedCall(
+        0.05, addLetter, [document.getElementById('name-surname'), chr]
+      )
+    );
+  }
+}
+
+function appearElements(ids, classes, timeout, duration) {
+  for (id of ids) {
+    console.log(id);
+  }
+  for (cls of classes) {
+    console.log(cls);
+  }
+}
+
+function addLetter(el, letter) {
+  el.textContent += letter;
 }
